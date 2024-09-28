100a528085acb512893d7309dab3868740919308
Aryan-Garg <aryangarg019@gmail.com>
2024-09-28 13:38:21 -0500

    Cosmetic/hparam tuning

--- scripts/tog_binary.sh
+++ scripts/tog_binary.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=1
 
+# HDRSplat args
+# python train.py -s /nobackup3/aryan/dataset/binary/f1000/train \
+#  --is_binary --position_lr_init 0.00001 --scaling_lr 0.0001 \
+#  --opacity_lr 0.001 --feature_lr 0.0001 --sh_degree 2
+
+# python train.py -s /nobackup3/aryan/dataset/binary/f1000/train --is_binary
+
 python train.py -s /nobackup3/aryan/dataset/binary/f1000/train \
- --is_binary --position_lr_init 0.00008 --scaling_lr 0.001 \
- --opacity_lr 0.005 --feature_lr 0.0005 --sh_degree 2
+    --start_checkpoint /nobackup3/aryan/HDRSplat/output/2e52f149-f/point_cloud/iteration_7000/point_cloud.ply --is_binary \
+    --position_lr_init 0.00001 --scaling_lr 0.0001 --opacity_lr 0.001 \
+    --feature_lr 0.0001 --sh_degree 2 --densify_until_iter 6999
--- scripts/tog_random_init_cloud.sh
+++ scripts/tog_random_init_cloud.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=1
 
-python train.py  -s /nobackup2/aryan/dataset/moped_rgb0025/train-colmap
+python train.py  -s /nobackup2/aryan/dataset/moped_rgb0025/train-imgs2 --save_iterations 7000
 
 # NOTE don't forget to rename transforms_train.json to transforms.json
